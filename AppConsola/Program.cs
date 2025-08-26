e6c6bc831b257f17ca8732919c438215a21204bd
Rodrigo <Rodrigo@DESKTOP-8Q8TEGQ>
2025-08-25 23:11:15 -0300

    Agregar archivos de proyecto.

---
+++ AppConsola/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AppConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
